081d30890c5023ee5626aea6c29dd9b99d3f250b
Lucas Frantz <maxkronus@gmail.com>
2021-07-17 20:38:23 -0300

    Streaming de Dados

---
+++ arquivos/uploadDeArquivos.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+
+fs.createReadStream('../assets/dog.jpg')
+    .pipe(fs.createWriteStream('../assets/dog-stream.jpg'))
+    .on('finish', () => {
+        console.log('imagem escrita')
+    })
