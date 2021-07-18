18839a5fc5a33b0106803ad9057d5892a447e38c
Lucas Frantz <maxkronus@gmail.com>
2021-07-18 13:01:50 -0300

    servicos externos

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -8,7 +8,7 @@ class Tabelas {
     }
 
     criarAtendimentos() {
-        let sql = 'CREATE TABLE IF NOT EXISTS Atendimentos( id int NOT NULL AUTO_INCREMENT,cliente varchar(50) NOT NULL,' +
+        let sql = 'CREATE TABLE IF NOT EXISTS Atendimentos( id int NOT NULL AUTO_INCREMENT,cliente varchar(11) NOT NULL,' +
             'pet varchar(20), data datetime NOT NULL, dataCriacao datetime NOT NULL,servico varchar(20) NOT NULL, ' +
             'status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
         this.conexao.query(sql, (erro) => {
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,4 +1,5 @@
 const moment = require('moment')
+const axios = require('axios')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
@@ -63,11 +64,14 @@ class Atendimento {
     buscaPorId(id, res) {
         let sql = `SELECT * FROM Atendimentos WHERE id=${id}`
 
-        conexao.query(sql, (erro, resultados) => {
+        conexao.query(sql, async (erro, resultados) => {
             const atendimento = resultados[0]
             if (erro) {
                 res.status(400).json(erro)
             } else {
+                const cpf = atendimento.cliente
+                const { data } = await axios.get(`http://localhost:8082/${cpf}`)
+                atendimento.cliente = data
                 res.status(200).json(atendimento)
             }
 
